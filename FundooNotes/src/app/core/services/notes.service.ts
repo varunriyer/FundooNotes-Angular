80578c307ea86898fd067c71495d105192a51940
varunriyer <rvaruniyer@gmail.com>
2025-06-02 05:15:07 +0530

    Add NotesService for managing notes with CRUD operations

---
+++ FundooNotes/src/app/core/services/notes.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+  private baseUrl = 'https://fundoonotes.incubation.bridgelabz.com';
+
+  constructor(private http: HttpClient) {}
+
+  private authHeader() {
+    const token = localStorage.getItem('token');
+    return {
+      headers: { Authorization: token ? `Bearer ${token}` : '' }
+    };
+  }
+
+  getNotesList() {
+    return this.http.get(`${this.baseUrl}/notes/getNotesList`, this.authHeader());
+  }
+
+  addNote(note: any) {
+    return this.http.post(`${this.baseUrl}/notes/addNotes`, note, this.authHeader());
+  }
+
+  updateNote(note: any) {
+    return this.http.post(`${this.baseUrl}/notes/updateNotes`, note, this.authHeader());
+  }
+
+  trashNote(note: any) {
+    return this.http.post(`${this.baseUrl}/notes/trashNotes`, note, this.authHeader());
+  }
+
+  deleteNote(note: any) {
+    return this.http.post(`${this.baseUrl}/notes/deleteForeverNotes`, note, this.authHeader());
+  }
+}
