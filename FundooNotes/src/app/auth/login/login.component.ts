7cdd4b6b9727366335ef27a7c4587e6d4fedf492
varunriyer <rvaruniyer@gmail.com>
2025-06-02 05:02:42 +0530

    Add login component with HTML, CSS, and TypeScript files

---
+++ FundooNotes/src/app/auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.id);
+        this.router.navigate(['/dashboard/notes']);
+      },
+      error: err => alert('Login failed')
+    });
+  }
+}
