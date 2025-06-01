f05412dd09547327240447d02e8278f4e594c693
varunriyer <rvaruniyer@gmail.com>
2025-06-02 05:07:10 +0530

    Add AuthService for user authentication with login, registration, and logout methods

---
+++ FundooNotes/src/app/core/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'https://fundoonotes.incubation.bridgelabz.com';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.baseUrl}/user/login`, { email, password });
+  }
+
+  register(data: any) {
+    return this.http.post(`${this.baseUrl}/user/userSignUp`, data);
+  }
+
+  logout() {
+    const token = localStorage.getItem('token');
+    return this.http.post(`${this.baseUrl}/user/logout`, {}, {
+      headers: { Authorization: token ? `Bearer ${token}` : '' }
+    });
+  }
+}
